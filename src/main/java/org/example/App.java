eca5d2f4a5bd2c3b92ea1ea4572e203242babb16
Kirill <justmymail0003@gmail.com>
2024-06-19 22:16:02 +0300

    new_branch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,6 +13,7 @@ public class App {
     public static void main(String[] args) {
         for(int i = 0;; i++) {
             System.out.println("Hello " + i);
+            //для теста емае
         }
     }
 
